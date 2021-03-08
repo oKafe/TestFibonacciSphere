cc43f6283443a77f032db309b2e18444b8877aa5
Andrii Syrota <andyorphandev@gmail.com>
2021-03-08 10:08:48 +0200

    Initial Commit

---
+++ TestFibonacciSphere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFibonacciSphere
+//
+//  Created by Andy Orphan on 08.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
